c0af01984f4e66512b8998edd2ed2efee4db9d2c
warin <killua891@gmail.com>
2023-07-24 15:09:13 +0700

    change title

--- index.php
+++ index.php
@@ -23,7 +23,7 @@
     <meta name="description" content="">
     <meta name="author" content="">
 
-    <title>Dashboard</title>
+    <title>Dashboard123</title>
 
     <!-- Custom fonts for this template-->
     <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
